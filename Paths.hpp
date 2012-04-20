06ca684e858ae89d9a3568419556ba3b3f6dcf07
Quentin Pradet <quentin.pradet@cea.fr>
2012-04-20 07:53:22 +0000

    most paths were recovered, but a few are missing. (wiktionaryelda, convfiles, FrameNetLUI/noms.list)

--- Paths.hpp
+++ Paths.hpp
@@ -15,39 +15,42 @@
 #define DATA_ADJ "/data/text/WordNet-3.0/dict/data.adj"
 */
 
-
-#define DICFILE "/data/mouton/CEA/data/FrameNetLUI/externals/dicfrenelda-utf8.txt"
+// missing
+#define DICFILE "/data/text/FrameNetLUI/externals/dicfrenelda-utf8.txt"
 #define DICFILE2 "/data/mouton/CEA/FNFR/FNFR.data/Wiktionaires/fr/frwiktionary-20090120-eldafmt.csv"
 
 
-#define INDEX_NOUN "/home/cm218888/opendata/WordNet-2.0/dict/index.noun"
-#define DATA_NOUN "/home/cm218888/opendata/WordNet-2.0/dict/data.noun"
+#define INDEX_NOUN "/data/text/moutonc/opendata/WordNet-2.0/dict/index.noun"
+#define DATA_NOUN "/data/text/moutonc/opendata/WordNet-2.0/dict/data.noun"
 
 
-#define INDEX_NOUN20 "/home/cm218888/opendata/WordNet-2.0/dict/index.noun"
-#define DATA_NOUN20 "/home/cm218888/opendata/WordNet-2.0/dict/data.noun"
+#define INDEX_NOUN20 "/data/text/moutonc/opendata/WordNet-2.0/dict/index.noun"
+#define DATA_NOUN20 "/data/text/moutonc/opendata/WordNet-2.0/dict/data.noun"
 
-#define INDEX_NOUN15 "/home/cm218888/opendata/WordNet-1.5/DICT/NOUN.IDX"
-#define DATA_NOUN15 "/home/cm218888/opendata/WordNet-1.5/DICT/NOUN.DAT"
+#define INDEX_NOUN15 "/data/text/quentin/opendata/WordNet-1.5/DICT/NOUN.IDX"
+#define DATA_NOUN15 "/data/text/quentin/opendata/WordNet-1.5/DICT/NOUN.DAT"
 
 #define NOUNS_LIST "/data/mouton/CEA/data/FrameNetLUI/externals/noms.list"
 
-#define INDEX_VERB "/home/cm218888/opendata/WordNet-3.0/dict/index.verb"
-#define DATA_VERB "/home/cm218888/opendata/WordNet-3.0/dict/data.verb"
-#define INDEX_ADJ "/home/cm218888/opendata/WordNet-3.0/dict/index.adj"
-#define DATA_ADJ "/home/cm218888/opendata/WordNet-3.0/dict/data.adj"
+#define INDEX_VERB "/data/text/moutonc/opendata/WordNet-3.0/dict/index.verb"
+#define DATA_VERB "/data/text/moutonc/opendata/WordNet-3.0/dict/data.verb"
+#define INDEX_ADJ "/data/text/moutonc/opendata/WordNet-3.0/dict/index.adj"
+#define DATA_ADJ "/data/text/moutonc/opendata/WordNet-3.0/dict/data.adj"
 
 // #define KNNSTDFILE "/data/CEA/data/knnall/$REL/SemMap.$REL.16384.knnResFile.$WORD.txt"
+// /home/myriam/.Trash-myriam/Class/vol/SemMap.COMPDUNOM.16384.knnResFile.5725.ext
+
+// missing
 #define KNNSTDFILE "/data/mouton/CEA/data/knnall/$REL/SemMap.$REL.16384.knnResFile.$WORD.txt"
 
-#define TYPEROLERFILE4 "/home/cm218888/otherdata/Resources/SemMap/windows/window10.dmp"
-#define TYPEROLERFILE3 "/home/cm218888/otherdata/Resources/SemMap/normal/COD_V.dmp"
-#define TYPEROLERFILE2 "/home/cm218888/otherdata/Resources/SemMap/normal/SUJ_V.dmp"
-#define TYPEROLERFILE "/home/cm218888/otherdata/Resources/SemMap/normal/COMPDUNOM.dmp"
-#define HOLOFILE "/home/cm218888/otherdata/Resources/SemMap/reverse/COMPDUNOM.reverse.dmp"
+#define TYPEROLERFILE4 "/data/text/FastKNN/Ressources/SemMap/windows/window10.dmp"
+#define TYPEROLERFILE3 "/data/text/FastKNN/Ressources/SemMap/normal/COD_V.dmp"
+#define TYPEROLERFILE2 "/data/text/FastKNN/Ressources/SemMap/normal/SUJ_V.dmp"
+#define TYPEROLERFILE "/data/text/FastKNN/Ressources/SemMap/normal/COMPDUNOM.dmp"
+#define HOLOFILE "/data/text/FastKNN/Ressources/SemMap/reverse/COMPDUNOM.reverse.dmp"
 
+// missing
 //#define CONVFILE "/data/text/FastKNN/bases/semmapconversion.global.bin"
-#define CONVFILE "/home/cm218888/otherdata/conversionfile.bin"
-
+#define CONVFILE "/data/text/moutonc/otherdata/conversionfile.bin"
 
 #endif
