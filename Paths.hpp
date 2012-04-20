b288a0b98e6bf4d581626e022de40958576bc7e0
Quentin Pradet <quentin.pradet@cea.fr>
2012-04-20 13:10:52 +0000

    add missing paths (thanks to Benjamin for the slocate help)

--- Paths.hpp
+++ Paths.hpp
@@ -1,8 +1,6 @@
 #ifndef PATHS_HPP
 #define PATHS_HPP
 
-
-
 //#define CONVFILE "/data/text/FastKNN/bases/semmapconversion.global.bin"
 //#define DICFILE "/data/text/FrameNetLUI/externals/dicfrenelda-utf8.txt"
 /*#define INDEX_NOUN "/data/text/WordNet-3.0/dict/index.nounk"
@@ -15,9 +13,8 @@
 #define DATA_ADJ "/data/text/WordNet-3.0/dict/data.adj"
 */
 
-// missing
 #define DICFILE "/data/text/FrameNetLUI/externals/dicfrenelda-utf8.txt"
-#define DICFILE2 "/data/mouton/CEA/FNFR/FNFR.data/Wiktionaires/fr/frwiktionary-20090120-eldafmt.csv"
+#define DICFILE2 "/data/text/FrameNetLUI/Wiktionaires/frwiktionary-20090120-eldafmt.csv"
 
 
 #define INDEX_NOUN "/data/text/moutonc/opendata/WordNet-2.0/dict/index.noun"
@@ -30,18 +27,14 @@
 #define INDEX_NOUN15 "/data/text/quentin/opendata/WordNet-1.5/DICT/NOUN.IDX"
 #define DATA_NOUN15 "/data/text/quentin/opendata/WordNet-1.5/DICT/NOUN.DAT"
 
-#define NOUNS_LIST "/data/mouton/CEA/data/FrameNetLUI/externals/noms.list"
+#define NOUNS_LIST "/data/text/FrameNetLUI/externals/POSlist/noms.list"
 
 #define INDEX_VERB "/data/text/moutonc/opendata/WordNet-3.0/dict/index.verb"
 #define DATA_VERB "/data/text/moutonc/opendata/WordNet-3.0/dict/data.verb"
 #define INDEX_ADJ "/data/text/moutonc/opendata/WordNet-3.0/dict/index.adj"
 #define DATA_ADJ "/data/text/moutonc/opendata/WordNet-3.0/dict/data.adj"
 
-// #define KNNSTDFILE "/data/CEA/data/knnall/$REL/SemMap.$REL.16384.knnResFile.$WORD.txt"
-// /home/myriam/.Trash-myriam/Class/vol/SemMap.COMPDUNOM.16384.knnResFile.5725.ext
-
-// missing
-#define KNNSTDFILE "/data/mouton/CEA/data/knnall/$REL/SemMap.$REL.16384.knnResFile.$WORD.txt"
+#define KNNSTDFILE "/data/text/FastKNN/knnall/$REL/SemMap.$REL.16384.knnResFile.$WORD.txt"
 
 #define TYPEROLERFILE4 "/data/text/FastKNN/Ressources/SemMap/windows/window10.dmp"
 #define TYPEROLERFILE3 "/data/text/FastKNN/Ressources/SemMap/normal/COD_V.dmp"
@@ -49,8 +42,6 @@
 #define TYPEROLERFILE "/data/text/FastKNN/Ressources/SemMap/normal/COMPDUNOM.dmp"
 #define HOLOFILE "/data/text/FastKNN/Ressources/SemMap/reverse/COMPDUNOM.reverse.dmp"
 
-// missing
-//#define CONVFILE "/data/text/FastKNN/bases/semmapconversion.global.bin"
-#define CONVFILE "/data/text/moutonc/otherdata/conversionfile.bin"
+#define CONVFILE "/data/text/FastKNN/bases.fr/semmapconversion.global.bin"
 
 #endif
