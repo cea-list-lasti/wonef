ded66ae86b0cbd4dd3d4d3dca78c677e6e1c96e8
Quentin Pradet <quentin.pradet@cea.fr>
2012-05-10 08:41:24 +0000

    whitespace, and "RAII": always follow a new with a delete: avoids segfaults.

--- translateWN.cpp
+++ translateWN.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-  
+
   time_t globalStart = time(NULL);
   time_t start = time(NULL);
 
@@ -24,11 +24,8 @@ int main(int argc, char **argv) {
   MeroHoloModule* meroholoer= NULL;
   MeroHoloLikeHyperModule* merohololiker= NULL;
   LastChanceModule* lastchancer= NULL;
-  
-
 
   cout << "Init duration : " << time(NULL) - start << " s " << endl;
-  
 
   bool nounonly = false;
   bool noen = false;
@@ -41,54 +38,54 @@ int main(int argc, char **argv) {
     cerr << "ARGV : '" << argv[i] << "'"<<endl;
     if (argv[i][0]=='n') {
       nounonly = true;
-      suffix += ".noun";     
+      suffix += ".noun";
     } else if (argv[i][0]=='E') {
       datafile = DATA_NOUN15;
-      suffix += ".ewn";     
+      suffix += ".ewn";
     } else if (argv[i][0]=='W') {
       datafile = DATA_NOUN20;
-      suffix += ".wolf";     
+      suffix += ".wolf";
     } else if (argv[i][0]=='1') {
-      suffix += "1";     
+      suffix += "1";
       seq.push_back(1);
     } else if (argv[i][0]=='2') {
-      suffix += "2";     
+      suffix += "2";
       seq.push_back(2);
     } else if (argv[i][0]=='3') {
-      suffix += "3";   
-      seq.push_back(3);  
+      suffix += "3";
+      seq.push_back(3);
     } else if (argv[i][0]=='4') {
-      suffix += "4"; 
-      seq.push_back(4);    
+      suffix += "4";
+      seq.push_back(4);
     } else if (argv[i][0]=='5') {
-      suffix += "5"; 
-      seq.push_back(5);    
+      suffix += "5";
+      seq.push_back(5);
     } else if (argv[i][0]=='6') {
-      suffix += "6"; 
-      seq.push_back(6);    
+      suffix += "6";
+      seq.push_back(6);
     } else if (argv[i][0]=='7') {
-      suffix += "7"; 
-      seq.push_back(7);    
+      suffix += "7";
+      seq.push_back(7);
     }  else if (argv[i][0]=='8') {
-      suffix += "8"; 
-      seq.push_back(8);    
+      suffix += "8";
+      seq.push_back(8);
     }  else if (argv[i][0]=='9') {
-      suffix += "9"; 
-      seq.push_back(9);    
+      suffix += "9";
+      seq.push_back(9);
     } else if (argv[i][0]=='N') {
       noen = true;
       stringstream ss ;
-      ss << "." << argv[i];     
+      ss << "." << argv[i];
       suffix += ss.str();
     }else {
       stringstream ss ;
-      ss << "." << argv[i];     
+      ss << "." << argv[i];
       suffix += ss.str();
     }
   }
-  
 
-  cout << "Init "  << endl;
+
+  cout << "Init " << suffix << endl;
   start = time(NULL);
   set<string> dicfiles;
   dicfiles.insert(DICFILE);
@@ -100,32 +97,27 @@ int main(int argc, char **argv) {
   for(vector<int>::iterator itseq = seq.begin(); itseq!= seq.end(); itseq++) {
     switch (*itseq) {
     case 1:
-      if (simsyner == NULL) {
-	simsyner = new SimSynModule;
-      }
+      simsyner = new SimSynModule;
 
       cout << "First step "  << endl;
       if (wn["00472185"].frenchCandidates.size()!=0) {
-	cerr << "TEST2 : " << wn["00472185"].frenchCandidates["coupling"].cand.size() << endl;
+          cerr << "TEST2 : " << wn["00472185"].frenchCandidates["coupling"].cand.size() << endl;
       }
       simsyner->process(wn);
       cout << "First step duration : " << time(NULL) - start << " s " << endl;
+      delete simsyner;
       break;
-      
-    case 2 : 
-      delete merohololiker;    
-      delete meroholoer;
-      delete hyperhypoer;
-      hyperhypoer = new HyperHypoModule(datafile, "COMPDUNOM");
 
+    case 2 :
+      hyperhypoer = new HyperHypoModule(datafile, "COMPDUNOM", R_HYPER);
       cout << "Second step "  << endl;
       start = time(NULL);
-      hyperhypoer->process(wn, R_HYPER);
+      hyperhypoer->process(wn);
       cout << "Second step duration : " << time(NULL) - start << " s " << endl;
+      delete hyperhypoer;
       break;
 
-      
-    case 3 : 
+    case 3 :
       /*
       if (hyperhypoer == NULL) {
 	hyperhypoer = new HyperHypoModule(datafile);
@@ -136,104 +128,79 @@ int main(int argc, char **argv) {
       cout << "Third step duration : " << time(NULL) - start << " s " << endl;
       */
 
-
-      if (merohololiker == NULL) {	
-	delete hyperhypoer;
-	delete meroholoer;
-	merohololiker = new MeroHoloLikeHyperModule(datafile);
-      }
+      merohololiker = new MeroHoloLikeHyperModule(datafile);
       cout << "Third step " << endl;
       start = time(NULL);
       merohololiker->process(wn, R_HYPER, false);
       cout << "Third step duration : " << time(NULL) - start << " s " << endl;
+      delete merohololiker;
       break;
 
-
-      break;
-
-    case 4 :      
-
-      if (meroholoer == NULL) {
-	delete hyperhypoer;
-	delete merohololiker;	
-	meroholoer = new MeroHoloModule(datafile);
-      }
+    case 4 :
+      meroholoer = new MeroHoloModule(datafile);
       cout << "Fourth step " << endl;
       start = time(NULL);
       meroholoer->process(wn);
       //      meroholoer->finalize();
       cout << "Fourth step duration : " << time(NULL) - start << " s " << endl;
+      delete meroholoer;
       break;
-  
-    case 5 : 
 
-      if (lastchancer == NULL) {
-	lastchancer = new LastChanceModule;
-      }
+    case 5 :
+      lastchancer = new LastChanceModule;
       cout << "Fifth step " << endl;
       start = time(NULL);
       lastchancer->process(wn);
       cout << "Fifth step duration : " << time(NULL) - start << " s " << endl;
+      delete lastchancer;
       break;
 
-
-      
-    case 6 : 
-
-      delete merohololiker;
-      delete hyperhypoer;
-      hyperhypoer = new HyperHypoModule(datafile, "SUJ_V");
-
+    case 6 :
+      hyperhypoer = new HyperHypoModule(datafile, "SUJ_V", R_HYPER);
       cout << "Six step "  << endl;
       start = time(NULL);
-      hyperhypoer->process(wn, R_HYPER, false);
+      hyperhypoer->process(wn, false);
       cout << "Six step duration : " << time(NULL) - start << " s " << endl;
+      delete hyperhypoer;
       break;
 
-    case 7 : 
-      delete merohololiker;
-      delete hyperhypoer;
-      hyperhypoer = new HyperHypoModule(datafile, "COMPDUNOM");
+    case 7 :
+      hyperhypoer = new HyperHypoModule(datafile, "COMPDUNOM", R_HYPO);
       cout << "Seventh step "  << endl;
       start = time(NULL);
-      hyperhypoer->process(wn, R_HYPO, false);
+      hyperhypoer->process(wn, false);
       cout << "Seventh step duration : " << time(NULL) - start << " s " << endl;
-      
-    case 8 : 
-
-
       delete hyperhypoer;
-      hyperhypoer = new HyperHypoModule(datafile, "COD_V");
-
-      cout << "Six step "  << endl;
-      start = time(NULL);
-      hyperhypoer->process(wn, R_HYPER, false);
-      cout << "Six step duration : " << time(NULL) - start << " s " << endl;
       break;
 
-    case 9 : 
-
-
-      delete hyperhypoer;
-      hyperhypoer = new HyperHypoModule(datafile, "window10");
-
-      cout << "Six step "  << endl;
+    case 8 :
+      hyperhypoer = new HyperHypoModule(datafile, "COD_V", R_HYPER);
+      cout << "Eigth step "  << endl;
       start = time(NULL);
-      hyperhypoer->process(wn, R_HYPER, false);
-      cout << "Six step duration : " << time(NULL) - start << " s " << endl;
+      hyperhypoer->process(wn, false);
+      cout << "Eigth step duration : " << time(NULL) - start << " s " << endl;
+      delete hyperhypoer;
       break;
-      
+
+    case 9 :
+      hyperhypoer = new HyperHypoModule(datafile, "window10", R_HYPER);
+      cout << "Ninth step "  << endl;
+      start = time(NULL);
+      hyperhypoer->process(wn, false);
+      cout << "Ninth step duration : " << time(NULL) - start << " s " << endl;
+      delete hyperhypoer;
+      break;
 
     default:
       break;
     }
 
   }
-  
+
   cout << "Print Index  " << endl;
   start = time(NULL);
   DumperModule dumper("data2/data.fr.nouns" + suffix, "data2/index.fr.noun" + suffix);
-  dumper.dump(wn, true);
+  dumper.dump(wn);
   cout << "Print index duration : " << time(NULL) - start << " s " << endl;
 
 
