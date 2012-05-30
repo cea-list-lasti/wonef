1a53f7312ad494ee3d092b7732060d45b0ea5e45
Quentin Pradet <quentin.pradet@cea.fr>
2012-05-30 09:35:52 +0000

    document translateNouns.sh

--- translateNouns.sh
+++ translateNouns.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-WOLF='/home/qp230782/projets/wolf/wolf-0.1.4.xml'
-POLYSEMOUSINDEX='/home/qp230782/projets/index.polysemous.noun'
-BCSMODE=4
-BCSFILE='/home/qp230782/projets/5000_bc.xml'
+# Usage
+# The parameters specify which iterations should be run. For example, with
+# ./translateNouns.sh 1 2 3 4, iterations 1 2 3 4 will be launched.
+#
+# Once finished, interesting files are:
+#  * data2/data.fr.nouns.wolf.nouns.Noen1234 (it's JAWS!)
+#  * logs/trans1234
+#  * logs/eval1234 (end of file contains results)
 
+
+
+
+# Reusing our above example, seqspaces will be "1 2 3 4" and seqs will be
+# "1234". Having $seqs is useful for filenames, while seqspaces is used to
+# launch translateWN.
 seqsspaces=$*
 seqs=${seqsspaces// /}
+
+
+WOLF='/home/qp230782/projets/wolf/wolf-0.1.4.xml'
+# This is simply index.noun without the monosemous nouns
+POLYSEMOUSINDEX='/home/qp230782/projets/index.polysemous.noun'
+# OLDSCHOOL (means I don't care about BCSFILE?)
+BCSMODE=4
+BCSFILE='/home/qp230782/projets/5000_bc.xml'
 
 echo "Translating... $seqsspaces"
 # It's really WOLF, not $WOLF
