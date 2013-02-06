f3f01e53b97d04077a0321971227782859f2f805
Quentin Pradet <quentin.pradet@cea.fr>
2013-02-06 09:13:19 +0000

    compareboth.py updated for adverbs

--- experiments/stats/compareboth.py
+++ experiments/stats/compareboth.py
@@ -3,22 +3,24 @@
 
 wolf, wonef = set(), set()
 
-for i in open('wolfpairs'): wolf.add(tuple(i.strip().split(':')))
-for i in open('wonefpairs'): wonef.add(tuple(i.strip().split(':')))
+for i in open('wolf10bpairs'): wolf.add(i.strip().split(':')[1])
+for i in open('wonefpairs'): wonef.add(i.strip().split(':')[1])
 
 inboth = len(wonef & wolf)
 
 wonefspecific = len(wonef - wolf)
 wolfspecific = len(wolf - wonef)
 
-#print(wonefspecific)
+print(wonefspecific)
 
 merge = wonef | wolf
-#print(len(merge))
-#print(len(wolf))
+print(len(merge))
+print(len(wolf))
 
-#print(100 * len(merge) / len(wolf))
-#print(100 * len(merge) / len(wonef))
+print(100 * len(merge) / len(wolf))
+print(100 * len(merge) / len(wonef))
+print("{:.2%}".format(len(wolf)/len(merge)))
+print("{:.2%}".format(len(wonef)/len(merge)))
 
-for s in (merge):
-  print(s)
+#for s in (merge):
+#  print(s)
