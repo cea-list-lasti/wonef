ebbc7a4bb64afaf6120e67b03a50cbac626450fd
Quentin Pradet <quentin.pradet@cea.fr>
2013-02-06 10:15:52 +0000

    stop if every translation is not found

--- Translate.cpp
+++ Translate.cpp
@@ -12,15 +12,22 @@ int main(int argc, char **argv) {
   Dictionaries dictionaries(pos);
   std::set<std::string> translations;
 
+  bool allFound = true;
   for (int i = 2; i < argc; i++) {
-    std::string original = argv[i];
+  std::string original = argv[i];
+  if (dictionaries.translations.find(original) == dictionaries.translations.end()) {
+      std::cerr << original <<  " not found!" << std::endl;
+      allFound = false;
+    }
 
     for (const std::string &translation : dictionaries.translations[original]) {
       translations.insert(translation);
     }
   }
 
-  std::cout << boost::algorithm::join(translations, ", ") << std::endl;
+  if (allFound) {
+    std::cout << boost::algorithm::join(translations, ", ") << std::endl;
+  }
 
   return 0;
 }
